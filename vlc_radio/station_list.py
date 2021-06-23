f86bdbcb5697057357a7f8e969b61b87dd97365c
Zachary Cutlip <uid000@gmail.com>
2021-06-23 10:50:05 -0700

    make station list a class for more flexibility

--- vlc_radio/station_list.py
+++ vlc_radio/station_list.py
@@ -3,20 +3,71 @@
 from csv import reader
 from importlib.resources import files
 
-
 from . import data
 
-urls = {}  # number selection: station URL
+
+DEFAULT_STATIONS_CSV = "stations.csv"
+# dark cyan; RGB-format text: 72, 201, 176
+ANSI_DK_CY = '\x1b[38;2;72;201;176m'
+ANSI_YELO = '\x1b[38;2;244;208;63m'  # yellow
+ANSI_DK_ORNG = '\x1b[38;2;220;118;51m'  # dark orange
+ANSI_RESET = '\x1b[0m'  # reset text format to default
 
 
-def get_station_list():
-    c1 = '\x1b[38;2;72;201;176m'  # dark cyan; RGB-format text: 72, 201, 176
-    c2 = '\x1b[38;2;244;208;63m'  # yellow
-    c3 = '\x1b[38;2;220;118;51m'  # dark orange
-    res = '\x1b[0m'  # reset text format to default
+class StationEntry:
 
-    with files(data).joinpath("stations.csv").open("r") as csv_file:
-        csv_rec_list = list(enumerate(reader(csv_file), 1))  # csv reader object
-        for num, rec in csv_rec_list:
-            print(f'{c1}{num:>2}{res}  {c2}{rec[0]:}{res}  {c3}{rec[1]}{res}')  # station list
-            urls[num] = rec[2]  # append number with URL to dictionary
+    def __init__(self, name, description, url):
+        self._name = name
+        self._description = description
+        self._url = url
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def url(self):
+        return self._url
+
+    def ansi_colorized(self):
+        c2 = ANSI_YELO
+        c3 = ANSI_DK_ORNG
+        res = ANSI_RESET
+
+        line = f'{c2}{self.name:}{res}  {c3}{self.description}{res}'
+        return line
+
+
+class StationList(dict):
+
+    def __init__(self):
+        super().__init__()
+        self._populate_stations()
+
+    def _populate_stations(self):
+        with files(data).joinpath(DEFAULT_STATIONS_CSV).open("r") as _file:
+            _reader = reader(_file)
+            for number, csv_record in enumerate(_reader, 1):
+                name = csv_record[0]
+                description = csv_record[1]
+                url = csv_record[2]
+                entry = StationEntry(name, description, url)
+                self[number] = entry
+
+    def ansi_colorized_line(self, number):
+        entry: StationEntry = self[number]
+        c1 = ANSI_DK_CY
+        res = ANSI_RESET
+        colorized_entry = entry.ansi_colorized()
+
+        line = f'{c1}{number:>2}{res}  {colorized_entry}'
+        return line
+
+    def print_menu(self):
+        for snum in self.keys():
+            line = self.ansi_colorized_line(snum)
+            print(line)
