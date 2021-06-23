b7aec1f9abcd703e89a7654803c2796544c71611
Zachary Cutlip <uid000@gmail.com>
2021-06-23 10:10:51 -0700

    load station list data from package

--- vlc_radio/station_list.py
+++ vlc_radio/station_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 '''Print color-schemed internet radio station list.'''
 from csv import reader
+from importlib.resources import files
 
+
+from . import data
 
 urls = {}  # number selection: station URL
 
@@ -12,7 +15,7 @@ def get_station_list():
     c3 = '\x1b[38;2;220;118;51m'  # dark orange
     res = '\x1b[0m'  # reset text format to default
 
-    with open('/scripts/py/vlc_radio_wrapper/data/stations.csv') as csv_file:
+    with files(data).joinpath("stations.csv").open("r") as csv_file:
         csv_rec_list = list(enumerate(reader(csv_file), 1))  # csv reader object
         for num, rec in csv_rec_list:
             print(f'{c1}{num:>2}{res}  {c2}{rec[0]:}{res}  {c3}{rec[1]}{res}')  # station list
