2113536d1d0bba3052c8389064be4c8b3885366e
Zachary Cutlip <uid000@gmail.com>
2021-06-23 09:42:18 -0700

    move app.py inside vlc_radio
    - slight refactor to be called from a console entrypoint

--- vlc_radio/app.py
+++ vlc_radio/app.py
@@ -2,14 +2,15 @@
 '''Play internet radio selection in VLC media player.
 Shift + M --> meta-info; q --> quit'''
 from subprocess import run
-from ascii_art import get_ascii_art
-import station_list
 
+from .ascii_art import get_ascii_art
+from .station_list import get_station_list
 
-print(get_ascii_art())  # get color-schemed ASCII art heading
-station_list.get_station_list()  # list stations
 
 def station_selection():
+    print(get_ascii_art())  # get color-schemed ASCII art heading
+    get_station_list()  # list stations
+
     '''Play selected internet radio station.'''
     urls = station_list.urls  # obj: number/station name/genres/url
     station_num = int(input('Enter item number: '))  # input station number
