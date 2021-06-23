372a5e77d679aeac80c2d80825c22f2365d03e87
Zachary Cutlip <uid000@gmail.com>
2021-06-23 10:27:46 -0700

    command-line option to not use ncurses

--- vlc_radio/app.py
+++ vlc_radio/app.py
@@ -3,10 +3,23 @@
 Shift + M --> meta-info; q --> quit'''
 import os
 
+from argparse import ArgumentParser
 from subprocess import run
 
 from .ascii_art import get_ascii_art
 from .station_list import get_station_list, urls
+
+
+def vlc_parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--no-curses",
+        help="Disable ncurses interface, run VLC in GUI mode",
+        action="store_true",
+    )
+
+    parsed = parser.parse_args()
+    return parsed
 
 
 def get_vlc_path():
@@ -25,12 +38,21 @@ def get_vlc_path():
 
 
 def station_selection():
+    options = vlc_parse_args()
     print(get_ascii_art())  # get color-schemed ASCII art heading
     get_station_list()  # list stations
     vlc_path = get_vlc_path()
     '''Play selected internet radio station.'''
     station_num = int(input('Enter item number: '))  # input station number
-    run([vlc_path, '--intf', 'ncurses', urls[station_num]], check=True)  # pass args
+
+    vlc_argv = [vlc_path]
+    if options.no_curses is False:
+        # Use curses unless user specified not to
+        # For some reason ncurses is broken on macOS + Apple Silicon + Homebrew VLC cask
+        vlc_argv.extend(['--intf', 'ncurses'])
+
+    vlc_argv.append(urls[station_num])
+    run(vlc_argv, check=True)  # pass args
 
 
 if __name__ == '__main__':
