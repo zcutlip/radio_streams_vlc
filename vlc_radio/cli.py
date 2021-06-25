ce22591c04e4f66e4352f812a877dfc17c4dee89
Zachary Cutlip <uid000@gmail.com>
2021-06-24 17:40:25 -0700

    capture and exit with return value from running VLC

--- vlc_radio/cli.py
+++ vlc_radio/cli.py
@@ -3,11 +3,11 @@ from .app import station_selection
 
 def main():
     try:
-        station_selection()
+        return station_selection()
     except KeyboardInterrupt:
         print("Keyboard interrupt. Quitting.")
         exit(0)
 
 
 if __name__ == "__main__":
-    main()
+    exit(main())
