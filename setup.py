a7dda03baf4ff8ca444cf7466350f1dec4ecb080
Zachary Cutlip <uid000@gmail.com>
2021-07-08 18:59:57 -0700

    update setup.py to reference chill_streams

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 about = {}
-with open("vlc_radio/__about__.py") as fp:
+with open("chill_streams/__about__.py") as fp:
     exec(fp.read(), about)
 
 with open("README.md", "r") as fp:
     long_description = fp.read()
 
 setup(
-    name="radio-streams-vlc",
+    name="chill-streams",
     version=about["__version__"],
     description=about["__summary__"],
     long_description=long_description,
@@ -19,9 +19,9 @@ setup(
     license="MIT",
     packages=find_packages(),
     entry_points={
-        "console_scripts": ["vlc-radio=vlc_radio.cli:main"],
+        "console_scripts": ["vlc-radio=chill_streams.cli:main"],
     },
     python_requires=">=3.7",
     install_requires=[],
-    package_data={"vlc_radio": ["data/*"]},
+    package_data={"chill_stations": ["data/*"]},
 )
